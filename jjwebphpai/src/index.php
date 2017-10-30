4e5252dbcbf58f07e3f73992affc8fdbf926385b
Jaroslav Jindrich <jajindri@microsoft.com>
2017-10-30 10:13:44 +0100

    AI key

--- jjwebphpai/src/index.php
+++ jjwebphpai/src/index.php
@@ -2,13 +2,13 @@
 <body>
 
 <h1>JJ Web PHP website</h1>
-<h3>using Application Insights</h3>
+<h3>using Application Insights v2</h3>
 
 <?php
 require_once 'vendor/autoload.php';
 
 $telemetryClient = new \ApplicationInsights\Telemetry_Client();
-$telemetryClient->getContext()->setInstrumentationKey('KEY');
+$telemetryClient->getContext()->setInstrumentationKey('6e06baa3-b2db-44bc-ba21-61442988822b');
 $telemetryClient->trackEvent('PHP container name: ' . gethostname());
 $telemetryClient->flush();
 $telemetryClient->trackRequest('myRequest', 'http://jj', time());

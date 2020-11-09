e873f903f7d92ab9e028d0c0c10003924c11f481
heoelri <heoelri@users.noreply.github.com>
2020-11-09 13:01:31 +0100

    Create config.inc.php

---
+++ jjwebphpmysql/src-php/config.inc.php
@@ -0,0 +1,12 @@
+<?php
+
+$host = 'jjtestmysql.mysql.database.azure.com';
+
+# with persistent connections
+# $host = 'p:jjtestmysql.mysql.database.azure.com';
+
+$username = 'jj@jjtestmysql';
+$password = 'Azure-12345';
+$db_name = 'jj';
+
+?>

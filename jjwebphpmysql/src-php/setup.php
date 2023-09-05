3b1febdb4e77fdfdd3eb51e52ae7eaef8c891d3d
Jaroslav Jindřich <jaroslav.jindrich@outlook.com>
2023-09-05 10:09:59 +0200

    Fix setup.php

--- jjwebphpmysql/src-php/setup.php
+++ jjwebphpmysql/src-php/setup.php
@@ -1,5 +1,5 @@
 <?php
-require("config.inc.php")
+require("config.inc.php");
 
 //Establishes the connection
 $conn = mysqli_init();
@@ -11,15 +11,14 @@ die('Failed to connect to MySQL: '.mysqli_connect_error());
 // Run the create table query
 if (mysqli_query($conn, '
 CREATE TABLE Products (
-`Id` INT NOT NULL AUTO_INCREMENT ,
-`ProductName` VARCHAR(200) NOT NULL ,
-`Color` VARCHAR(50) NOT NULL ,
-`Price` DOUBLE NOT NULL ,
-PRIMARY KEY (`Id`)
+'Id' INT NOT NULL AUTO_INCREMENT ,
+'ProductName' VARCHAR(200) NOT NULL ,
+'Color' VARCHAR(50) NOT NULL ,
+'Price' DOUBLE NOT NULL ,
+PRIMARY KEY ('Id')
 );
 ')) {
 printf("Table created\n");
-}
 
 //Close the connection
 mysqli_close($conn);
